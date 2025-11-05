1021e21a2c9baedb2cea33c03518e2c4e3b037ff
Amirreza Noruzi <amirrezanoruziiii@gmail.com>
2025-11-05 20:22:25 +0330

    design home page

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="font-dyna container">
     <HeaderMenu />
     <slot />
   </div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,92 @@
+<script setup lang="ts">
+import { ref, onMounted, watch, nextTick } from 'vue';
+import { gsap } from 'gsap';
+import logo from "~/../public/logo.png";
+import SplitText from 'gsap/SplitText';
+
+const phrases = [
+    'Frontend Developer',
+    'Nuxt.js/Vue.js Developer',
+    'Data Science Enthusiast'
+];
+const currentPhrase = ref(0);
+
+
+gsap.registerPlugin(SplitText);
+
+const introRef = ref<HTMLElement | null>(null);
+const phraseRef = ref<HTMLElement | null>(null)
+
+
+onMounted(() => {
+  document.fonts.ready.then(() => {
+    if (introRef.value) {
+      gsap.set(introRef.value, { opacity: 1 });
+      const split = SplitText.create(introRef.value, {
+        type: 'chars, words',
+        mask: 'chars'
+      });
+
+      gsap.from(split.chars, {
+        duration: 0.6,
+        yPercent: 'random([-150, 150])',
+        xPercent: 'random([-150, 150])',
+        stagger: {
+          from: 'random',
+          amount: 0.6
+        },
+        ease: 'power3.out'
+      });
+    }
+
+    setInterval(() => {
+      currentPhrase.value = (currentPhrase.value + 1) % phrases.length;
+    }, 3000);
+
+  });
+
+});
+
+
+watch(currentPhrase, async () => {
+  await nextTick(); // wait for DOM update
+  if (phraseRef.value) {
+    const split = SplitText.create(phraseRef.value, {
+      type: 'chars',
+      mask: 'chars',
+
+    });
+
+    gsap.from(split.chars, {
+      duration: 0.6,
+      yPercent: 'random([-150, 150])',
+      xPercent: 'random([-150, 150])',
+      stagger: {
+        from: 'random',
+        amount: 0.6
+      },
+      ease: 'power3.out'
+    });
+  }
+});
+</script>
+
 <template>
-    <div>
-        <h1 class=" text-4xl text-center text-main1">firstpage</h1>
+    <div class="grid justify-center md:flex items-center pb-15 h-screen space-x-7">
+            <div class=" w-full h-full flex justify-center items-center">
+                <img :src="logo" class="w-full" alt="Amirreza Noruzi Logo">
+            </div>
+
+            <div class="w-full h-full flex flex-col justify-center items-center text-center space-y-4">
+                <h1 ref="introRef" class=" inline-block text-3xl  md:text-6xl font-semibold opacity-0 md:leading-20">
+                    Hi. I'm <br><span class=" text-cyan-400">Amirreza Noruzi</span>
+                    <br><span class=" text-xl md:text-2xl">a <span  ref="phraseRef" class=" text-cyan-600">{{ phrases[currentPhrase] }}</span></span>
+                </h1>
+            </div>
     </div>
 
 </template>
+
 
 
 
